4ef0a9163280f28df4ded7d88d7ebe065b4cdaeb
Auraum <aldomi04@ucm.es>
2023-03-08 12:12:36 +0100

    launch.bat and custom formats

---
+++ config/custom-formats.ts
@@ -0,0 +1,23 @@
+// Note: This is the list of formats
+// The rules that formats use are stored in data/rulesets.ts
+
+export const Formats: FormatList = [
+	{
+		section: "Singles in your area",
+		column: 5,
+	},
+	{
+		name: "[Gen 4] Ice Platinum",
+		desc: `beat up`,
+		mod: 'gen4',
+		ruleset: ['Standard'],
+		banlist: ['AG', 'Uber'],
+	},
+	{
+		name: "[Gen 5] White Illusion",
+		desc: `where where are the gen 5 abilities`,
+		mod: 'gen5',
+		ruleset: ['Standard'],
+		banlist: ['AG', 'Uber'],
+	},
+];
