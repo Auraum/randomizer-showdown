1c38b8febabafd22451254969bd2761ca8342382
Auraum <aldomi04@ucm.es>
2023-03-09 01:46:45 +0100

    white illusion rayquaza

---
+++ data/mods/whiteillusion/pokedex.ts
@@ -0,0 +1,8 @@
+export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+	rayquaza: {
+		inherit: true,
+		types: ["Ice"],
+		baseStats: { hp: 84, atk: 228, def: 11, spa: 151, spd: 19, spe: 188 },
+		abilities: { 0: "Minus" },
+	},
+};
