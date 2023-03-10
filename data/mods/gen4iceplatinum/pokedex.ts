e2a98c6702864dd722bb4644fced71ac41ba0be1
Auraum <aldomi04@ucm.es>
2023-03-10 15:00:24 +0100

    try bulbasaur dex

--- data/mods/gen4iceplatinum/pokedex.ts
+++ data/mods/gen4iceplatinum/pokedex.ts
@@ -5,4 +5,10 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 		baseStats: { hp: 84, atk: 228, def: 11, spa: 151, spd: 19, spe: 188 },
 		abilities: { 0: "Minus" },
 	},
+	bulbasaur: {
+		inherit: true,
+		types: ["Ghost"],
+		baseStats: { hp: 84, atk: 228, def: 11, spa: 151, spd: 19, spe: 188 },
+		abilities: { 0: "Minus" },
+	},
 };
