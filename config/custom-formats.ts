32f316e1efe6579e749c6abc2bb28194683a4c14
Auraum <aldomi04@ucm.es>
2023-03-11 15:18:30 +0100

    random battle and rulesets

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -7,14 +7,21 @@ export const Formats: FormatList = [
 		column: 5,
 	},
 	{
-		name: "[Gen 4] Ice Platinum",
+		name: "[Gen 4] Ice Platinum Random Battle",
+		desc: `beat up`,
+		mod: 'gen4iceplatinum',
+		team: 'random',
+		ruleset: ['Standard'],
+	},
+	{
+		name: "[Gen 4] Ice Platinum OU",
 		desc: `beat up`,
 		mod: 'gen4iceplatinum',
 		ruleset: ['Standard'],
 		banlist: ['AG', 'Uber'],
 	},
 	{
-		name: "[Gen 5] White Illusion",
+		name: "[Gen 5] White Illusion OU",
 		desc: `where where are the gen 5 abilities`,
 		mod: 'gen5whiteillusion',
 		ruleset: ['Standard'],
