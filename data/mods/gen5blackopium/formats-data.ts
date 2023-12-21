1f5bb4cc1ac378c80a8e736652d329ce43678bf5
Auraum <aldomi04@ucm.es>
2023-12-21 17:00:10 +0100

    added black opium

---
+++ data/mods/gen5blackopium/formats-data.ts
@@ -0,0 +1,22 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	tornadustherian: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	thundurustherian: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	landorustherian: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	kyuremblack: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	kyuremwhite: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+};
