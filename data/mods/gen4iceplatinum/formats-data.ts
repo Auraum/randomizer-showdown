a933fb86cc3cb6d34421c162393cbb6b5cc24fba
Auraum <aldomi04@ucm.es>
2023-12-25 01:07:23 +0100

    illegal v2

--- data/mods/gen4iceplatinum/formats-data.ts
+++ data/mods/gen4iceplatinum/formats-data.ts
@@ -1,89 +1,118 @@
 export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 	deoxysattack: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	deoxysdefense: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	deoxysspeed: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	wormadamsandy: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	wormadamtrash: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	cherrimsunshine: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	rotomheat: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	rotomwash: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	rotomfrost: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	rotomfan: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	rotommow: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	giratinaorigin: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	shayminsky: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	arceusbug: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	arceusdark: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	arceusdragon: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	arceuselectric: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	arceusfighting: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	arceusfire: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	arceusflying: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	arceusghost: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	arceusgrass: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	arceusground: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	arceusice: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	arceuspoison: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	arceuspsychic: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	arceusrock: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	arceussteel: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 	arceuswater: {
+		isNonstandard: "Future",
 		tier: "Illegal",
 	},
 };
